f23f8119f10194747d117683ff2921f9219f7b60
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2022-05-16 13:21:21 +0200

    Add TetWildPlugin.

---
+++ TetWildPlugin/TetWildPlugin.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+
+#if defined(TETWILDPLUGIN_EXPORTS)
+#define TETWILD_API __declspec(dllexport) 
+#elif defined(TETWILDPLUGIN_IMPORTS)
+#define TETWILD_API __declspec(dllimport)
+#else
+#define TETWILD_API
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	TETWILD_API int TetWildPlugin_GetVersion()
+	{
+		return 1;
+	}
+
+	TETWILD_API void TetWildPlugin_TetrahedralizeMesh()
+	{
+		tetwild::Args args;
+
+		Eigen::MatrixXd VI, VO;
+		Eigen::MatrixXi FI, TO;
+		Eigen::VectorXd AO;
+
+		tetwild::tetrahedralization(VI, FI, VO, TO, AO, args);
+	}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ TetWildPlugin/pch.h
@@ -0,0 +1,18 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
+// Windows Header Files
+#include <Windows.h>
+
+#include <tetwild/tetwild.h>
+
+#endif //PCH_H
